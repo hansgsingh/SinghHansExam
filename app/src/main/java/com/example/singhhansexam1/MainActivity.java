8f727392d05778266136889adb0e9a594ade3765
Hans Singh <hansemmanuele.singh.iics@ust.edu.ph>
2019-09-14 12:23:59 +0800

    2

--- app/src/main/java/com/example/singhhansexam1/MainActivity.java
+++ app/src/main/java/com/example/singhhansexam1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.singhhansexam1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView note1 = (TextView) findViewById(R.id.textView7);
+        note1.setBackgroundColor(Color.parseColor("#AA3456"));
     }
 }
